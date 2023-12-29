366bda746e6f3d2ba57e23514b1c4050446ecddb
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2023-12-29 12:17:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
  import mongoose from "mongoose";
-  
 
 const connectDB = async ()=>{
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/E-commerce")
+        mongoose.connect("process.env.MONGO_URL")
 .then(()=>{
     console.log("database connected")
 })
